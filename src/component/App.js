62c5f4a435f9457a3d33ee7c1656d9951c1d32bb
johnnynode <johnnynode@gmail.com>
2018-10-06 21:56:59 +0800

    repo init

---
+++ src/component/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="component-app">
+        drag
+      </div>
+    );
+  }
+}
